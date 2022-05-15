c16221da69123a02950b872b21180789633d664b
khsk <eatparalytic@gmail.com>
2022-05-15 12:34:15 +0900

    fix current directory bug #1
    作業フォルダーが変わった場合にfontsを参照できない問題を修正

--- localfont.cpp
+++ localfont.cpp
@@ -46,12 +46,33 @@ BOOL WINAPI DllMain( HINSTANCE hInstance,DWORD dwNotification,LPVOID lpReserved 
     switch (dwNotification) {
         case DLL_PROCESS_ATTACH:
             // exe直下でもplugins内設置でもカレントはaviutl.exe直下になる
-            if (!PathIsDirectory(FONTS_DIR)) {
-                MessageBox(NULL, TEXT("Fonts directory is missing"),NULL, MB_OK);
+            // ただしGUIからのexeの直接起動と異なりショートカット経由や外部呼出しではカレントは異なる場合があるため、相対パスではAviUtil内のfontsを参照できなかったりカレント内の意図しないfontsフォルダを参照してしまう場合があるのでこれは行わない
+
+//            if (PathIsDirectory(FONTS_DIR)) {
+//                setFontFiles(FONTS_DIR);
+//                return TRUE;
+//            }
+
+            // カレントを使用せずに実行中ファイルのパスからAviUtl直下を算出する
+            TCHAR modulePath[MAX_PATH];
+            if (!GetModuleFileName(0, modulePath, MAX_PATH)) {
+                MessageBox(NULL, TEXT("Error: GetModuleFileName"),NULL, MB_OK);
                 return TRUE;
             }
-            setFontFiles(FONTS_DIR);
-    }
+            if(PathRemoveFileSpec(modulePath) == 0) {
+                MessageBox(NULL, TEXT("Error: PathRemoveFileSpec"),NULL, MB_OK);
+                MessageBox(NULL, TEXT(modulePath),NULL, MB_OK);
+                return TRUE;
+            }
+            std::string modulePathString = modulePath;
+            std::string fontsFullPath = modulePathString + "\\" + FONTS_DIR;
+            if (!PathIsDirectory(fontsFullPath.c_str())) {
+                MessageBox(NULL, TEXT("Fonts directory is missing"),NULL, MB_OK);
+                MessageBox(NULL, fontsFullPath.c_str(),NULL, MB_OK);
+            } else {
+                setFontFiles(fontsFullPath);
+            }
+        }
     return TRUE;
 }
 
